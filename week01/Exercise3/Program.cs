2995ff3f7e35ee7b2b53e98c9ed9df1fc072e102
haltermoney <chalterman@byupathway.edu>
2025-11-02 21:41:45 -0700

    Completed exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int guesses = 0;
+        int guess = 0;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+            guesses++;
+
+            if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {guesses} guesses.");
+            }
+
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
+
     }
 }
