5829e7705850b5c73a1501dee42f4b36f5c8be1d
haltermoney <chalterman@byupathway.edu>
2025-12-05 22:33:08 -0700

    Week 05 learning

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.Clear();
+
+        Assignment student = new Assignment("Cory", "Algebra");
+        string summary = student.GetSummary();
+        Console.WriteLine(summary);
+        Console.WriteLine();
+
+        MathAssignment student2 = new MathAssignment("James", "Geometry", "7.4", "1-10 all odd");
+        summary = student2.GetSummary();
+        Console.WriteLine(summary);
+        string homework = student2.GetHomeworkList();
+        Console.WriteLine(homework);
+        Console.WriteLine();
+
+        WritingAssignment student3 = new WritingAssignment("Hannah", "English", "A Clockwork Orange");
+        summary = student3.GetSummary();
+        Console.WriteLine(summary);
+        homework = student3.GetWritingInformation();
+        Console.WriteLine(homework);
+        Console.WriteLine();
+
     }
 }
